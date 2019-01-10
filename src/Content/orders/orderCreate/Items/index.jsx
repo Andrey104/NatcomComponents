45014198d1c0c20a852d3006c7a88b11019b056f
andrey104 <andreyrs104@gmail.com>
2019-01-10 11:20:09 +0300

    hot fix order table

--- src/Content/orders/orderCreate/Items/index.jsx
+++ src/Content/orders/orderCreate/Items/index.jsx
@@ -192,7 +192,7 @@ export default class extends React.Component {
         const {items} = this.props;
         this.items = items;
         return (this.items.map((item, index) => (
-                <tr key={item.item.item + index}>
+                <tr key={item.item.id + index + item.item.name}>
                     <th scope="row">
                         <div className="number-block">
                             <img className="del-button"
