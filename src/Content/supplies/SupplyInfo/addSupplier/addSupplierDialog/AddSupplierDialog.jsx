2eb6e2dabcde485a73d7e950a44f6d6c20148119
andrey104 <andreyrs104@gmail.com>
2019-05-29 20:14:23 +0300

    fix scroll in supply create

--- src/Content/supplies/SupplyInfo/addSupplier/addSupplierDialog/AddSupplierDialog.jsx
+++ src/Content/supplies/SupplyInfo/addSupplier/addSupplierDialog/AddSupplierDialog.jsx
@@ -3,15 +3,14 @@ import classNames from 'classnames/bind';
 
 import DialogWindow from '../../../../../components/ModalWindow/index';
 import SuppliersList from '../../../../suppliers/suppliersPage/suppliersList/SuppliersList';
-import styles from './styles.scss';
+import styles from './styles.css';
 
-let cx = classNames.bind(styles);
 
 class AddSupplierDialog extends React.Component {
 
     render() {
         return (
-            <div className={cx('modal-body')}>
+            <div className = 'modal-body'>
                 <SuppliersList match={this.props.match}
                                supplierForSupply={this.props.supplierForSupply}/>
             </div>
